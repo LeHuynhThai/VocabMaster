aa096a24d950a4c803d2989f86fa623d8fb00097
Raikouxyz <thailh0902@gmail.com>
2025-07-12 13:02:50 +0700

    up

---
+++ Controllers/WordGeneratorController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VocabMaster.Data;
+
+namespace VocabMaster.Controllers
+{
+    public class WordGeneratorController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public WordGeneratorController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        private static readonly List<string> Words = new List<string>
+        {
+            "apple", "banana", "cat", "dog", "elephant", "flower", "guitar", "house", "island", "jungle"
+        };
+
+        [HttpGet]
+        public IActionResult Index(string randomWord = null)
+        {
+            ViewBag.RandomWord = randomWord;   
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult GenerateWord()
+        {
+            var random = new Random();
+            var randomWord = Words[random.Next(Words.Count)];
+            return RedirectToAction("Index", new { randomWord = randomWord });
+        }
+    }
+}
