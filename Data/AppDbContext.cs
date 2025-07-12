d8466a92c55b1ba02d36dccdcdf74b1e660c5617
Raikouxyz <thailh0902@gmail.com>
2025-07-12 13:28:32 +0700

    up

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -6,6 +6,7 @@ namespace VocabMaster.Data
     public class AppDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<Vocabulary> Vocabularies { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
         {
