189c4935d7fd0d07b0e5534d9df35fb17fcea883
LeHuynhThai <thailh0902@gmail.com>
2025-10-15 01:35:44 +0700

    up

--- Service/Implementation/QuizzQuestionService.cs
+++ Service/Implementation/QuizzQuestionService.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Repository.Entities;
+using Repository.Interfaces;
+using Service.Interfaces;
 
-namespace Services.Implementation
+namespace Service.Implementation
 {
-    internal class QuizzQuestionService
+    public class QuizzQuestionService : IQuizzQuestionService
     {
+        private readonly IQuizzQuestionRepo _quizzQuestionRepo;
+
+        public QuizzQuestionService(IQuizzQuestionRepo quizzQuestionRepo)
+        {
+            _quizzQuestionRepo = quizzQuestionRepo;
+        }
+
+        public async Task<QuizQuestion?> GetRandomUncompletedQuestion(int userId)
+        {
+            return await _quizzQuestionRepo.GetRandomUncompletedQuestion(userId);
+        }
     }
 }
--- Service/Interfaces/IQuizzQuestionService.cs
+++ Service/Interfaces/IQuizzQuestionService.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Repository.Entities;
 
-namespace Services.Interfaces
+namespace Service.Interfaces
 {
     public interface IQuizzQuestionService
     {
+        Task<QuizQuestion?> GetRandomUncompletedQuestion(int userId);
     }
 }
